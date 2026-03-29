1cd3eee4b8114715c4fd32f8a7e49a28edfaaa82
jnaneswari <jnaneswarikodavati46@gmail.com>
2026-03-29 13:13:42 +0530

    Final: 100% Stable WhiteNoise Configuration and Robust ONNX Engine

--- Bone_Abnormality_Detection/settings.py
+++ Bone_Abnormality_Detection/settings.py
@@ -140,7 +140,7 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
 MEDIA_URL = '/media/'
