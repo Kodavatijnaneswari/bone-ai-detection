fdddd28f7564a21f0a540b915440344f5fb99f65
jnaneswari <jnaneswarikodavati46@gmail.com>
2026-03-29 11:36:14 +0530

    Final: End-to-End Success - Added Production Logging, Security Headers, and Authentication Message Feedback

--- Bone_Abnormality_Detection/settings.py
+++ Bone_Abnormality_Detection/settings.py
@@ -134,10 +134,23 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+# Final Production Security & Logging
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = os.environ.get('RENDER', 'False') == 'True'
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+}
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
