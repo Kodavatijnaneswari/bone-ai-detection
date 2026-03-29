ea87543d216e24c3aec9cd7bfa0d58534dbd3d56
jnaneswari <jnaneswarikodavati46@gmail.com>
2026-03-29 10:42:55 +0530

    Fix: Renamed module Wrist_Abnormality_Detection to Bone_Abnormality_Detection to resolve Render deployment error

--- Bone_Abnormality_Detection/urls.py
+++ Bone_Abnormality_Detection/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Wrist_Abnormality_Detection project.
+URL configuration for Bone_Abnormality_Detection project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
--- Bone_Abnormality_Detection/wsgi.py
+++ Bone_Abnormality_Detection/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Wrist_Abnormality_Detection.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Bone_Abnormality_Detection.settings')
 
 application = get_wsgi_application()
